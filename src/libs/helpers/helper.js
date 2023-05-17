87ccacf06b40a0b17445011d7b2e94039aeaeb5e
wlankasper <asu.artur.smirnov@gmail.com>
2023-05-17 09:02:37 +0200

    inter products

--- src/libs/helpers/helper.js
+++ src/libs/helpers/helper.js
@@ -1,1 +1,33 @@
 // empty
+
+const libraries = [];
+
+libraries.push({
+    name: 'Grande biblioteca',
+    address: 'Via del Ronco, 7, 34133',
+    items: {
+        books: [
+            {
+                isbn: '231242312',
+                title: 'The Hobbit',
+                authors: ['J.R.R. Tolkien'],
+                published: '2001/02/21',
+                publishingHouse: 'Home of books',
+            },
+            {
+                isbn: '231242312',
+                title: 'The Hobbit',
+                authors: ['J.R.R. Tolkien'],
+                published: '2001/02/21',
+                publishingHouse: 'Home of books',
+            },
+            {
+                isbn: '231242312',
+                title: 'The Hobbit',
+                authors: ['J.R.R. Tolkien'],
+                published: '2001/02/21',
+                publishingHouse: 'Home of books',
+            },
+        ]
+    }
+});
