6ddfa27b2bf52604e25f2106ef019a50ba3a5a1d
wlankasper <asu.artur.smirnov@gmail.com>
2023-05-19 13:37:17 +0200

    beta item card

--- src/libs/components/item/script.js
+++ src/libs/components/item/script.js
@@ -1,1 +1,78 @@
-// empty
+import { componentUtils } from "../../utils/componentUtils.js";
+
+class Item {
+    constructor(parentElement, props) {
+        this.parentElement = parentElement;
+        this.props = props;
+
+        this.elements = {};
+    }
+
+    init() {
+        this.initTemplate();
+        this.initElements();
+        this.initEventListeners();
+        this.render();
+    }
+
+    initTemplate() {
+        const { imgPath, title, author, published, publisher, isbn, available } = this.props;
+
+        this.template = `<section id="_template">
+            <div class="item-container">
+                <div class="item-front">
+                    <img src="${imgPath}" alt="image of a book">
+                </div>
+                <div class="item-back">
+                    <div class="heading">
+                        <h3 class="text">
+                            ${title}
+                        </h3>
+                    </div>
+                    <div class="content">
+                        <h5 class="text">
+                            Autore: <span class="text text-accent-extend">${author}<span/>
+                        </h5>
+                        <h5 class="text">
+                            Publicato: <span class="text text-accent-extend">${published}<span/>
+                        </h5>
+                        <h5 class="text">
+                            Casa editrice: <span class="text text-accent-extend">${publisher}<span/>
+                        </h5>
+                        <h5 class="text">
+                            ISBN: <span class="text text-accent-extend">${isbn}<span/>
+                        </h5>
+                    </div>
+                    <div class="footer">
+                        ${available ? `<h5 class="text">Disponibile dal: <span class="text text-accent">${available}<span/></h5>` : ''}
+                        <button class="button-item">
+                            <h3 class="text">
+                                Prenota
+                            </h3>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>`;
+    }
+
+    initElements() {
+        this.rootElement = componentUtils.htmlDOMConverter(this.template);
+
+        this.elements = {
+            buttonItem: this.rootElement.querySelector('button.button-item'),
+        };
+    }
+
+    initEventListeners() {
+
+    }
+
+    render() {
+        if (!this.rootElement) return;
+
+        componentUtils.render(this.parentElement, this.rootElement);
+    }
+}
+
+export default Item;
--- src/routes/script.js
+++ src/routes/script.js
@@ -1,9 +1,40 @@
 import Header from "../libs/components/header/script.js";
+import Item from "../libs/components/item/script.js";
 
 const sections = {
     header: document.querySelector('section#header'),
     heading: document.querySelector('section#heading'),
+    itemList: document.querySelector('.item-list'),
 }
 
 const header = new Header(sections.header)
 header.init();
+
+
+const props = { imgPath: 'https://m.media-amazon.com/images/I/710+HcoP38L._AC_UF1000,1000_QL80_.jpg', title: 'The Hobbit', author: 'J.R.R. Tolkien', published: '2001/02/21', publisher: 'Home of books', isbn: '687356654', available: '21/06/2022' };
+const itemTest = new Item(sections.itemList, props);
+itemTest.init();
+
+const itemTest1 = new Item(sections.itemList, props);
+itemTest1.init();
+
+const itemTest2 = new Item(sections.itemList, props);
+itemTest2.init();
+
+const itemTest3 = new Item(sections.itemList, props);
+itemTest1.init();
+
+const itemTest4 = new Item(sections.itemList, props);
+itemTest2.init();
+
+const itemTest5 = new Item(sections.itemList, props);
+itemTest1.init();
+
+const itemTest6 = new Item(sections.itemList, props);
+itemTest2.init();
+
+const itemTest7 = new Item(sections.itemList, props);
+itemTest1.init();
+
+const itemTest8 = new Item(sections.itemList, props);
+itemTest2.init();
