9d8fd70534891bdccb5295ec6068c701366a03c0
wlankasper <asu.artur.smirnov@gmail.com>
2023-05-22 13:04:39 +0200

    beta table layout

--- src/routes/admin/script.js
+++ src/routes/admin/script.js
@@ -1,1 +1,8 @@
-// empty
+import Header from "../../libs/components/header/script.js";
+
+const sections = {
+    header: document.querySelector('section#header'),
+}
+
+const header = new Header(sections.header)
+header.init();
