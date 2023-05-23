ab57cbf2a5e4b5792541322f252a05cff96e3c30
wlankasper <asu.artur.smirnov@gmail.com>
2023-05-23 21:33:40 +0200

    layout sign in/up

--- src/libs/components/header/script.js
+++ src/libs/components/header/script.js
@@ -18,12 +18,14 @@ class Header {
         this.template = `<section id="_template">
             <header>
                 <div class="header-block block-logo">
-                    <span class="title-logo">
-                        <span class="text text-title text-accent">B</span>
-                        <span class="text text-title text-accent-extend">B</span>
-                    </span>
+                    <a href="/"> //todo
+                        <span class="title-logo">
+                            <span class="text text-title text-accent">B</span>
+                            <span class="text text-title text-accent-extend">B</span>
+                        </span>
+                    </a>
                 </div>
-                <div></div>
+                <div class="menu"></div>
                 <div class="header-block block-account">
                     <button class="button-heading button-sign button-extend hidden">
                         <h3 class="text text-accent">
@@ -44,18 +46,29 @@ class Header {
         this.rootElement = componentUtils.htmlDOMConverter(this.template);
 
         this.elements = {
+            header: this.rootElement.querySelector('header'),
+            menu: this.rootElement.querySelector('.menu'),
+            account: this.rootElement.querySelector('.block-account'),
             buttonSign: this.rootElement.querySelector('button.button-sign'),
             buttonExit: this.rootElement.querySelector('button.button-exit'),
         };
 
         const isSignedIn = false;
 
-        this.elements.buttonSign.classList.toggle('hidden', isSignedIn);
-        this.elements.buttonExit.classList.toggle('hidden', !isSignedIn);
+        if (window.location.href.toString().includes('sign')) {
+            this.elements.menu.classList.toggle('hidden', true);
+            this.elements.account.classList.toggle('hidden', true);
+            this.elements.header.style = "justify-content: center;";
+        } else {
+            this.elements.buttonSign.classList.toggle('hidden', isSignedIn);
+            this.elements.buttonExit.classList.toggle('hidden', !isSignedIn);
+        }
     }
 
     initEventListeners() {
-
+        this.elements.buttonSign.addEventListener('click', () => {
+            window.location.href = './sign_in/index.html';
+        })
     }
 
     render() {
